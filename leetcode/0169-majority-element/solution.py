e555cdb8c2c8500ae7352cfec0031fd51914e32b
IrishMorales <irishdmorales@xolveit.com>
2025-02-09 14:53:30 +0000

    [leetcode-sync] Runtime - 4 ms (71.11%), Memory - 19.6 MB (18.30%)

---
+++ leetcode/0169-majority-element/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        majority = nums[0]
+        count = 1
+
+        for num in nums[1:]:
+            if count == 0:
+                majority = num
+            
+            if num != majority:
+                count -= 1
+            else:
+                count += 1
+
+        return majority
+        
