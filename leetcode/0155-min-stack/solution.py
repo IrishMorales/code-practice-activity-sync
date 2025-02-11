9fb849dcd22676fb8d58be6a35a6af659f17b113
IrishMorales <irishdmorales@xolveit.com>
2025-02-11 09:22:17 +0000

    [leetcode-sync] Runtime - 15 ms (14.19%), Memory - 22.3 MB (6.54%)

--- leetcode/0155-min-stack/solution.py
+++ leetcode/0155-min-stack/solution.py
@@ -1,27 +1,32 @@
 class MinStack:
     def __init__(self):
-        self.head = None
+        # initialize a linked list where head = top of stack
+        self.head = self.Node()
 
-    def push(self, val: int) -> None:
-        if self.head == None:
-            self.head = self.Node(val, val, None)
-        else:
-            self.head = self.Node(val, min(val, self.head.min_val), self.head)
+    # each insert into the linked list will have the value itself and the minimum recorded so far
+    def push(self, val):
+        # if the value itself is lower than the minimum, minimum for that node = value
+        min_val = min(val, self.head.min_val) if self.head.min_val is not None else val
+        new_head = self.Node(val, self.head, min_val)
+        self.head = new_head
 
-    def pop(self) -> None:
-        self.head = self.head.next_node
+    # pop = remove node at top of stack, set new head to head.next
+    def pop(self):
+        self.head = self.head.next
 
+    # top = return head.val
     def top(self) -> int:
         return self.head.val
 
+    # min = return head.min
     def getMin(self) -> int:
         return self.head.min_val
 
     class Node:
-        def __init__(self, val: int, min_val: int, next_node):
+        def __init__(self, val=None, next=None, min_val=None):
             self.val = val
+            self.next = next
             self.min_val = min_val
-            self.next_node = next_node
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
